9c12f683cc26daec37d0136dee824df9411bc681
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-04-22 20:46:47 +0400

    cloned last tasks folder

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ import { SectionCreator } from "./join-us-section.js";
 const factory = new SectionCreator;
 const standard = factory.create('standard');
 
-
 // const adv = factory.create('advanced');
 // factory.remove();
