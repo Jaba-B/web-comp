508975bbdbb0703bf2bf08307c984a17227056be
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-04-22 20:48:44 +0400

    added email validation functionality and imported in main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { SectionCreator } from "./join-us-section.js";
+import { validator } from "./src/email-validator.js";
 
 const factory = new SectionCreator;
 const standard = factory.create('standard');
+
 
 // const adv = factory.create('advanced');
 // factory.remove();
---
+++ src/email-validator.js
@@ -0,0 +1,18 @@
+export const validator = setTimeout(()=> {
+    let parent = document.getElementById('app-container');
+    let btn = parent.children[4].children[0].children[2].children[1];
+    let input = parent.children[4].children[0].children[2].children[0];
+    btn.addEventListener('click', () => {
+        const validEmailEndings = ['gmail.com', 'outlook.com', 'yandex.ru'];
+        const email = input.value;
+        const ending = email.substring(email.indexOf('@') + 1);
+        const start = email.substring(0,email.indexOf('@'));
+        if(ending === validEmailEndings[0] && start.length > 0 
+            || ending === validEmailEndings[1] && start.length > 0 
+            || ending === validEmailEndings[2] && start.length > 0) {
+            alert('true');
+        } else {
+            alert('false');
+        }
+    }, false);
+}, 1000);
