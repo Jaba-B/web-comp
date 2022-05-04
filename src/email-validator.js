aea562cb46bf92b3234b0696e923e5d7e50b6bea
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-05-04 14:07:38 +0400

    added functionality to change styles when clicking on the button

--- src/email-validator.js
+++ src/email-validator.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-mixed-operators */
 export const validator = setTimeout(() => {
   const parent = document.getElementById('app-container');
+  const form = parent.children[4].children[0].children[2];
   const btn = parent.children[4].children[0].children[2].children[1];
   const input = parent.children[4].children[0].children[2].children[0];
   btn.addEventListener('click', () => {
@@ -14,6 +15,19 @@ export const validator = setTimeout(() => {
             || ending === validEmailEndings[1] && start.length > 0
             || ending === validEmailEndings[2] && start.length > 0) {
       localStorage.setItem('email', email);
+      if (btn.innerHTML == 'SUBSCRIBE') {
+        input.style.display = 'none';
+        btn.style.height = '42px';
+        btn.innerHTML = 'UNSUBSCRIBE';
+        form.style.justifyContent = 'center';
+      } else {
+        input.style.display = 'inline-block';
+        btn.style.height = '42px';
+        btn.innerHTML = 'SUBSCRIBE';
+        form.style.justifyContent = 'space-between';
+        localStorage.removeItem('email');
+        input.value = '';
+      }
     } else {
       alert('false');
     }
