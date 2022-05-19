1a0a6d65675b3bdbd17d248914230bfbbd1c80ad
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-05-19 18:17:29 +0400

    create first simple test and environement for testing

---
+++ src/tests/emailValidator.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-undef */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { expect } from 'chai';
+import { validator } from '../email-validator.js';
+
+describe('simple test', () => {
+  it('should return str', () => {
+    expect('str').to.equal('str');
+  });
+});
