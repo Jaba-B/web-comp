be3a7ed7ab8e87ada1d9ee5c7f503bbcd3064f3d
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-06-09 13:28:58 +0400

    installed nyc and added script coverage

--- src/tests/emailValidator.test.js
+++ src/tests/emailValidator.test.js
@@ -10,148 +10,148 @@ import { assert } from 'chai';
 import  sinon  from 'sinon';
 import pkg from 'mocha-sinon';
 const {mochasinon} = pkg;
-import { validator, validate, validateAsync, validateWithThrow, validateWithLog } from '../email-validator.js';
+import { validator, validate, validateAsync, validateWithThrow, validateWithLog} from '../email-validator.js';
 
-// describe('simple test', () => {
-//   it('should return str', () => {
-//     expect('str').to.equal('str');
-//   });
-// });
+describe('simple test', () => {
+  it('should return str', () => {
+    expect('str').to.equal('str');
+  });
+});
 
-// describe('validate basic functionality', () => {
-//   it('empty input value should return false', () => {
-//     const expected = false;
-//     const actual = validate('');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('ops@gmail.com should return true', () => {
-//     const expected = true;
-//     const actual = validate('ops@gmail.com');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('ops@outlook.com should return true', () => {
-//     const expected = true;
-//     const actual = validate('ops@outlook.com');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('ops@yandex.ru should return true', () => {
-//     const expected = true;
-//     const actual = validate('ops@yandex.ru');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('opsyandex.ru should return false', () => {
-//     const expected = false;
-//     const actual = validate('opsyandex.ru');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('ops@outlook should return false', () => {
-//     const expected = false;
-//     const actual = validate('ops@outlook');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('@outlook.com should return false', () => {
-//     const expected = false;
-//     const actual = validate('@outlook.com');
-//     expect(actual).to.deep.equal(expected);
-//   });
-//   it('o@outlook.com should return false', () => {
-//     const expected = false;
-//     const actual = validate('o@outlook.com');
-//     expect(actual).to.deep.equal(expected);
-//   });
-// });
+describe('validate basic functionality', () => {
+  it('empty input value should return false', () => {
+    const expected = false;
+    const actual = validate('');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('ops@gmail.com should return true', () => {
+    const expected = true;
+    const actual = validate('ops@gmail.com');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('ops@outlook.com should return true', () => {
+    const expected = true;
+    const actual = validate('ops@outlook.com');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('ops@yandex.ru should return true', () => {
+    const expected = true;
+    const actual = validate('ops@yandex.ru');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('opsyandex.ru should return false', () => {
+    const expected = false;
+    const actual = validate('opsyandex.ru');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('ops@outlook should return false', () => {
+    const expected = false;
+    const actual = validate('ops@outlook');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('@outlook.com should return false', () => {
+    const expected = false;
+    const actual = validate('@outlook.com');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('o@outlook.com should return true', () => {
+    const expected = true;
+    const actual = validate('o@outlook.com');
+    expect(actual).to.deep.equal(expected);
+  });
+});
 
-// describe('test validateAsync basic functionality', () => {
-//   it('oops@gmail.com should return true', function ()  {
-//     validateAsync('oops@gmail.com').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@outlook.com should return true', function ()  {
-//     validateAsync('oops@outlook.com').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@yandex.ru should return true', function ()  {
-//     validateAsync('oops@yandex.ru').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@yandex.com should return false', function ()  {
-//     validateAsync('oops@yandex.com').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@outlook.ru should return false', function ()  {
-//     validateAsync('oops@outlook.ru').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@gmail.ru should return false', function ()  {
-//     validateAsync('oops@gmail.ru').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@mail.ru should return false', function ()  {
-//     validateAsync('oops@mail.ru').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-//   it('oops@outlook. should return false', function ()  {
-//     validateAsync('oops@outlook.').then((result) => {
-//       expect(result).to.equal(true)
-//     }).catch((result) => {
-//       expect(result).to.equal(false)
-//     })
-//   })
-// });
+describe('test validateAsync basic functionality', () => {
+  it('oops@gmail.com should return true', function ()  {
+    validateAsync('oops@gmail.com').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@outlook.com should return true', function ()  {
+    validateAsync('oops@outlook.com').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@yandex.ru should return true', function ()  {
+    validateAsync('oops@yandex.ru').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@yandex.com should return false', function ()  {
+    validateAsync('oops@yandex.com').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@outlook.ru should return false', function ()  {
+    validateAsync('oops@outlook.ru').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@gmail.ru should return false', function ()  {
+    validateAsync('oops@gmail.ru').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@mail.ru should return false', function ()  {
+    validateAsync('oops@mail.ru').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+  it('oops@outlook. should return false', function ()  {
+    validateAsync('oops@outlook.').then((result) => {
+      expect(result).to.equal(true)
+    }).catch((result) => {
+      expect(result).to.equal(false)
+    })
+  })
+});
 
-// describe('test validateWithThrow basic functionality', () => {
-//     it ('oop@mail.com should throw error', () => {
-//         assert.throws(function() { validateWithThrow('oops@mail.com') }, Error, 'email is invalid');
-//     })
-//     it('oops@yandex.com should return false', function ()  {
-//         assert.throws(function() { validateWithThrow('oops@yandex.com') }, Error, 'email is invalid');
-//     })
-//     it('oops@outlook.ru should return false', function ()  {
-//         assert.throws(function() { validateWithThrow('oops@outlook.ru') }, Error, 'email is invalid');
-//     })
-//     it('oops@gmail.ru should return false', function ()  {
-//         assert.throws(function() { validateWithThrow('oops@gmail.ru') }, Error, 'email is invalid');
-//     })
-//     it('oops@mail.ru should return false', function ()  {
-//         assert.throws(function() { validateWithThrow('oops@mail.ru') }, Error, 'email is invalid');
-//     })
-//     it('oops@outlook. should return false', function ()  {
-//         assert.throws(function() { validateWithThrow('oops@outlook.') }, Error, 'email is invalid');
-//     })
-//     it('oops@gmail.com should return true', function ()  {
-//         const result = validateWithThrow('oops@gmail.com')
-//         expect(result).to.equal(true)
-//     })
-//     it('oops@outlook.com should return true', function ()  {
-//         const result = validateWithThrow('oops@outlook.com')
-//         expect(result).to.equal(true)
-//     })
-//     it('oops@yandex.ru should return true', function ()  {
-//         const result = validateWithThrow('oops@yandex.ru')
-//         expect(result).to.equal(true)
-//     })
-// })
+describe('test validateWithThrow basic functionality', () => {
+    it ('oop@mail.com should throw error', () => {
+        assert.throws(function() { validateWithThrow('oops@mail.com') }, Error, 'email is invalid');
+    })
+    it('oops@yandex.com should return false', function ()  {
+        assert.throws(function() { validateWithThrow('oops@yandex.com') }, Error, 'email is invalid');
+    })
+    it('oops@outlook.ru should return false', function ()  {
+        assert.throws(function() { validateWithThrow('oops@outlook.ru') }, Error, 'email is invalid');
+    })
+    it('oops@gmail.ru should return false', function ()  {
+        assert.throws(function() { validateWithThrow('oops@gmail.ru') }, Error, 'email is invalid');
+    })
+    it('oops@mail.ru should return false', function ()  {
+        assert.throws(function() { validateWithThrow('oops@mail.ru') }, Error, 'email is invalid');
+    })
+    it('oops@outlook. should return false', function ()  {
+        assert.throws(function() { validateWithThrow('oops@outlook.') }, Error, 'email is invalid');
+    })
+    it('oops@gmail.com should return true', function ()  {
+        const result = validateWithThrow('oops@gmail.com')
+        expect(result).to.equal(true)
+    })
+    it('oops@outlook.com should return true', function ()  {
+        const result = validateWithThrow('oops@outlook.com')
+        expect(result).to.equal(true)
+    })
+    it('oops@yandex.ru should return true', function ()  {
+        const result = validateWithThrow('oops@yandex.ru')
+        expect(result).to.equal(true)
+    })
+})
 
 
 describe('validateWithLog basic functionality', () => {
@@ -204,11 +204,11 @@ describe('validateWithLog basic functionality', () => {
     expect(validateWithLog('@outlook.com')).to.deep.equal(false);
     stub.restore();
   });
-  it('o@outlook.com should return false', async () => {
+  it('o@outlook.com should return true', async () => {
     const stub = sinon.stub(console, 'log');
     validateWithLog('o@outlook.com');
     expect( console.log.calledWith('o@outlook.com') ).to.be.true;
-    expect(validateWithLog('o@outlook.com')).to.deep.equal(false);
+    expect(validateWithLog('o@outlook.com')).to.deep.equal(true);
     stub.restore();
   });
 });
