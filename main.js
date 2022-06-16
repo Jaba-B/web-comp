b81db2a4206a3383f9da2c1ce66e101f97a8312d
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-06-16 19:30:24 +0400

    to improve performance moved join our section to html instead of creating from js also changed classes(advanced standart) to simple functions

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
-import { SectionCreator } from './join-us-section.js';
-import { validator } from './src/email-validator.js';
+/* eslint-disable no-mixed-operators */
+// import { SectionCreator } from './join-us-section.js';
+// import { validator } from './src/email-validator.js';
 import './styles/style.css';
 
-const factory = new SectionCreator();
-const standard = factory.create('standard');
+const form = document.querySelector('added-section__email-box');
+const btn = document.getElementById('subscribeBtn');
+const h2 = document.getElementById('h2');
+btn.addEventListener('click', () => {
+  const input = document.getElementById('email-input');
+  const validEmailEndings = ['gmail.com', 'outlook.com', 'yandex.ru'];
+  const email = input.value;
+  const ending = email.substring(email.indexOf('@') + 1);
+  const start = email.substring(0, email.indexOf('@'));
+  if (ending === validEmailEndings[0] && start.length > 0
+            || ending === validEmailEndings[1] && start.length > 0
+            || ending === validEmailEndings[2] && start.length > 0) {
+    localStorage.setItem('email', email);
+    if (btn.innerHTML == 'SUBSCRIBE') {
+      input.style.display = 'none';
+      btn.style.height = '42px';
+      btn.innerHTML = 'UNSUBSCRIBE';
+      form.style.justifyContent = 'center';
+    } else {
+      input.style.display = 'inline-block';
+      btn.style.height = '42px';
+      btn.innerHTML = 'SUBSCRIBE';
+      form.style.justifyContent = 'space-between';
+      localStorage.removeItem('email');
+      input.value = '';
+    }
+  } else {
+    alert('false');
+  }
+  const emailInput = document.querySelector('added-section__email-btn');
+  emailInput.style.display = 'none';
+}, false);
 
-// const adv = factory.create('advanced');
-// factory.remove();
+const advanced = () => {
+  btn.textContent = 'Subscribe to Advanced Program';
+  h2.textContent = 'Join Our Advanced Program';
+};
+
+const remove = () => {
+  const parent = document.getElementById('app-container');
+  const child = parent.children[4];
+  child.remove();
+};
+
+// advanced();
+// remove();
